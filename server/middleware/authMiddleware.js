87a0157084be4c848e6d49254c9b83f55f2db31e
Huzaifa <hquaid796@gmail.com>
2024-10-02 20:24:59 +0500

    Removed Previous implementations

--- server/middleware/authMiddleware.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req, res, next) => {
-    if (req.isAuthenticated()) {
-      
-      return next(); // User is authenticated, proceed to the next middleware or route handler
-    }  
-    res.json({message:'not authenticated'}); // If not authenticated, redirect to the login page
-  };
-  
