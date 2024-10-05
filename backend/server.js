5bcb631c29c78e5928765dcbeaa652e066765e44
Huzaifa <72322343+huzaifaWD20@users.noreply.github.com>
2024-10-05 23:18:45 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,7 @@ app.use(cors({
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 5000
 })
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('Error connecting to MongoDB:', err));
